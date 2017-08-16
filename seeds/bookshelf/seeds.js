48bddd683c1ad2880bd6607884ee1bb2e5a823be
pxw0685 <PAT_WOOWONG@homedepot.com>
2017-08-16 15:30:22 -0400

    Added where clause

--- seeds/bookshelf/seeds.js
+++ seeds/bookshelf/seeds.js
@@ -11,24 +11,30 @@ async function seed() {
   await Movie.where('id', '!=', 0).destroy();
   await Director.where('id', '!=', 0).destroy();
 
-  const CN = await new Director({ first_name: 'Christopher', last_name: 'Nolan' }).save();
-
-  const [inception, batman] = await Promise.all([
-    CN.movies().create(new Movie({ name: 'Inception', release_date: '2016-01-04' })),
-    CN.movies().create(new Movie({ name: 'Batman Begins', release_date: '2008-01-04' }))
+  const [CN, MB] = await Promise.all([
+    new Director({ first_name: 'Christopher', last_name: 'Nolan' }).save(),
+    new Director({ first_name: 'Michael', last_name: 'Bay' }).save()
   ]);
 
   const [LD, CB] = await Promise.all([
-    new Actor({ first_name: 'Leonardo', last_name: 'Di Caprio' }),
-    new Actor({ first_name: 'Christian', last_name: 'Bale' })
+    new Actor({ first_name: 'Leonardo', last_name: 'Di Caprio' }).save(),
+    new Actor({ first_name: 'Christian', last_name: 'Bale' }).save()
+  ]);
+
+  await Promise.all([
+    CN.movies().create(new Movie({ name: 'Inception', release_date: '2016-01-04' })),
+    CN.movies().create(new Movie({ name: 'Batman Begins', release_date: '2008-01-04' })),
+    MB.movies().create(new Movie({ name: 'Transformers', release_date: '2008-01-04' }))
   ]);
 
   const results = await Promise.all([
-    inception.actors().attach(LD),
-    batman.actors().attach(CB)
+    LD.movies().attach(await Movie.where('name', '=', 'Inception').fetch()),    // inception.actors().attach(LD)
+    CB.movies().attach(await Movie.where('name', '=', 'Batman Begins').fetch())     // batman.actors().attach(CB)
   ]);
 
-  await Movie.fetchAll({ withRelated: ['actors','director'] })
+  // await transformers.set({ name: 'Transformers2' }).save()
+
+  await Movie.where('release_date', '<', "2010-01-01").fetchAll({ withRelated: ['actors', 'director'] })
   .then(movies => movies.forEach(movie => console.log(movie.toJSON())));
   process.exit(0);  // disconnect from database
 }
