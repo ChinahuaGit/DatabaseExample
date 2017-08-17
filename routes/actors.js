909dee13b01d0218f242d15695cbb105ebbf3c26
pxw0685 <PAT_WOOWONG@homedepot.com>
2017-08-17 12:43:38 -0400

    Refactored async.

--- routes/actors.js
+++ routes/actors.js
@@ -1,11 +1,48 @@
-var express = require('express');
-var router = express.Router();
-var Directors = require('../models/directors')
+const express = require('express');
+const Actors = require('../models/actors');
+
+const router = express.Router();
+
+async function getActor(id) {
+  return Actors.where({ id: id }).fetch();
+}
+
+async function addActor(d) {
+  return new Actors(d).save();
+}
+
+async function updateActor(id, body) {
+  let actor = await Actors.where({
+    id: id
+  }).fetch();
+
+  if (actor != null) {
+    const updates = {};
+
+    if (body.first_name) {
+      updates.first_name = body.first_name;
+    }
+
+    if (body.last_name) {
+      updates.last_name = body.last_name;
+    }
+
+    return actor.set(updates).save();
+  }
+
+  return false;
+}
+
+async function deleteActor(id) {
+  return Actors.where({
+    id: id
+  }).destory();
+}
 
 // GET ALL THE DIRECTORS
 router.get('/', function(req, res, next) {
-  Directors.fetchAll().then(directors => {
-    res.json(directors.toJSON());
+  Actors.fetchAll().then(actors => {
+      res.json(actors.toJSON());
   })
   .catch(error => {
     next(error);
@@ -14,56 +51,40 @@ router.get('/', function(req, res, next) {
 
 // CREATE A NEW DIRECTOR
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  new Directors(req.body).save();
-  res.status(200).send(`Director: ${req.body.first_name} ${req.body.last_name} Added`);
+  addActor(req.body).then((d) => {
+    res.status(200).send(`Actor: ${req.body.first_name} ${req.body.last_name} Added`);
+  });
 });
 
 // UPDATE A DIRECTOR WITH ID=?
 router.patch('/:id', function(req, res, next) {
-  Directors.where({ id: req.params.id }).fetch()
-  .then((director) => {
-    if (director !== null) {
-      let updates = {};
-
-      if (req.body.first_name) {
-        updates.first_name = req.body.first_name;
-      }
-      if (req.body.last_name) {
-        updates.last_name = req.body.last_name;
-      }
-
-      director.set(updates).save().then(() => {
-        res.status(200).send(`Director(${req.params.id}): ${req.body.first_name} ${req.body.last_name} Updated`);
-      });
+  updateActor(req.params.id, req.body)
+  .then((d) => {
+    if (d) {
+      res.status(200).send(`Actor(${req.params.id}) Updated`);
     } else {
-      res.status(404).send("not found");
+      res.status(404).send('not found');
     }
   });
 });
 
 // DELETE A DIRECTOR WITH ID=?
 router.delete('/:id', function(req, res, next) {
-  Directors.where({ id: req.params.id }).destroy()
-  .then(() => {
-    res.status(200).send("Director deleted.");
+  deleteActor(req.params.id)
+  .then((d) => {
+    res.status(200).send("Actor deleted.");
   });
 });
 
 // GET A DIRECTOR WITH ID=?
 router.get('/:id', function(req, res, next) {
-  Directors.where({
-    id: req.params.id
-  }).fetch()
-  .then(director => {
-    if (director == null) {
+  getActor(req.params.id)
+  .then((d) => {
+    if (d == null) {
       res.status(404).send("not found");
     } else {
-      res.json(director.toJSON());
+      res.json(d.toJSON());
     }
-  })
-  .catch(error => {
-    next(error);
   });
 });
 
--- routes/directors.js
+++ routes/directors.js
@@ -2,68 +2,88 @@ var express = require('express');
 var router = express.Router();
 var Directors = require('../models/directors')
 
+async function getDirector(id) {
+  return Directors.where({ id: id }).fetch();
+}
+
+async function addDirector(d) {
+  return new Directors(d).save();
+}
+
+async function updateDirector(id, body) {
+  let director = await Directors.where({
+    id: id
+  }).fetch();
+
+  if (director != null) {
+    const updates = {};
+
+    if (body.first_name) {
+      updates.first_name = body.first_name;
+    }
+
+    if (body.last_name) {
+      updates.last_name = body.last_name;
+    }
+
+    return director.set(updates).save();
+  }
+
+  return false;
+}
+
+async function deleteDirector(id) {
+  return Directors.where({
+    id: id
+  }).destory();
+}
+
 // GET ALL THE DIRECTORS
 router.get('/', function(req, res, next) {
   Directors.fetchAll().then(directors => {
-    res.json(directors.toJSON());
-  })
-  .catch(error => {
-    next(error);
-  });
+      res.json(directors.toJSON());
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 // CREATE A NEW DIRECTOR
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  new Directors(req.body).save();
-  res.status(200).send(`Director: ${req.body.first_name} ${req.body.last_name} Added`);
+  addDirector(req.body).then((d) => {
+    res.status(200).send(`Director: ${req.body.first_name} ${req.body.last_name} Added`);
+  });
 });
 
 // UPDATE A DIRECTOR WITH ID=?
 router.patch('/:id', function(req, res, next) {
-  Directors.where({ id: req.params.id }).fetch()
-  .then((director) => {
-    if (director !== null) {
-      let updates = {};
-
-      if (req.body.first_name) {
-        updates.first_name = req.body.first_name;
-      }
-      if (req.body.last_name) {
-        updates.last_name = req.body.last_name;
-      }
-
-      director.set(updates).save().then(() => {
-        res.status(200).send(`Director(${req.params.id}): ${req.body.first_name} ${req.body.last_name} Updated`);
-      });
+  updateDirector(req.params.id, req.body)
+  .then((d) => {
+    if (d) {
+      res.status(200).send(`Director(${req.params.id}) Updated`);
     } else {
-      res.status(404).send("not found");
+      res.status(404).send('not found');
     }
   });
 });
 
 // DELETE A DIRECTOR WITH ID=?
 router.delete('/:id', function(req, res, next) {
-  Directors.where({ id: req.params.id }).destroy()
-  .then(() => {
+  deleteDirector(req.params.id)
+  .then((d) => {
     res.status(200).send("Director deleted.");
   });
 });
 
 // GET A DIRECTOR WITH ID=?
 router.get('/:id', function(req, res, next) {
-  Directors.where({
-    id: req.params.id
-  }).fetch()
-  .then(director => {
-    if (director == null) {
+  getDirector(req.params.id)
+  .then((d) => {
+    if (d == null) {
       res.status(404).send("not found");
     } else {
-      res.json(director.toJSON());
+      res.json(d.toJSON());
     }
-  })
-  .catch(error => {
-    next(error);
   });
 });
 
