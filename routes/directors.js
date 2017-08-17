8e17047067b97ebdcc5651b160301b1edcaeee5e
pxw0685 <PAT_WOOWONG@homedepot.com>
2017-08-17 10:12:21 -0400

    updates

--- routes/directors.js
+++ routes/directors.js
@@ -18,7 +18,7 @@ router.post('/', function(req, res, next) {
 });
 
 router.delete('/:id', function(req, res, next) {
-  Directors.where({ id: req.params.id }).destory()
+  Directors.where({ id: req.params.id }).destroy()
   .then(() => {
     res.status(200).send("Director deleted.");
   });
